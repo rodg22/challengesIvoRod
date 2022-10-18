304910f01359c1e21057c04b4907cf9b49f12ca2
Rodrigo Giudice <giudicerodrigo@hotmail.com>
2022-10-18 09:54:25 -0300

    Trying stat filtering

--- src/components/Pages/PokeGrilla/PokeGrilla.js
+++ src/components/Pages/PokeGrilla/PokeGrilla.js
@@ -19,12 +19,60 @@ export const PokeGrilla = () => {
     types === '' ? setData(pokeData) : setData(filteredPokeData)
   }, [filteredPokeData, pokeData, types])
 
+  // Sort original
   data.sort((a, b) => {
     return a.id - b.id
   })
 
+  //Logica de ordenar el array por HP
+  // data.sort((a, b) => {
+  //   return b.stats[0]['base_stat'] - a.stats[0]['base_stat']
+  // })
+  const ordenarData = (posicion) => {
+    data.sort((a, b) => {
+      return b.stats[posicion]['base_stat'] - a.stats[posicion]['base_stat']
+    })
+  }
+
+  const [clickedStat, setClickedStat] = useState('')
+
+  switch (clickedStat) {
+    case 'HP':
+      ordenarData(0)
+      break
+    case 'ATTACK':
+      ordenarData(1)
+      break
+    case 'DEFENSE':
+      ordenarData(2)
+      break
+    case 'SPECIAL ATTACK':
+      ordenarData(3)
+      break
+    case 'SPECIAL DEFENSE':
+      ordenarData(4)
+      break
+    case 'SPEED':
+      ordenarData(5)
+      break
+    default:
+      break
+  }
+
   return (
     <>
+      {/* Filtrar por stat: HP, Attack, Defense, S.a, S.d, Speed.
+      De mayor a menor --> pagination? Renderizar de a 20
+
+      Data viene ordenada por id: del 1 al 1043
+      Agarrar esa data y filtrar por el stat elegido de mayor a menor (filter con sort)
+
+      Setear ese estado en setData 
+      
+      Nosotros nos traemos la data, que viene de a 20 en 58 paginas
+      Se tienen que acomodar de tal forma que quede de mayor a menos por stat
+      
+      */}
       <h1>POKEGRID</h1>
       {data.length ? (
         <>
