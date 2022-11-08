11572389eda7dc441c0c80454fda6af9a17ddf06
Rodrigo Giudice <giudicerodrigo@hotmail.com>
2022-11-08 09:51:10 -0300

    Change PokeScreen mockup
    We were changing the individual pokemons mockup

--- src/components/Pages/PokeGrilla/PokeScreen.js
+++ src/components/Pages/PokeGrilla/PokeScreen.js
@@ -12,7 +12,7 @@ export const PokeScreen = () => {
   const [showMoves, setShowMoves] = useState(false);
   const [showStats, setShowStats] = useState(false);
   const [showAbility, setShowAbility] = useState(false);
-  
+
   const handleReturn = (e) => {
     navigate(-1);
   };
@@ -20,7 +20,7 @@ export const PokeScreen = () => {
   const handleMovesOpen = (e) => {
     showMoves ? setShowMoves(false) : setShowMoves(true);
   };
-  
+
   const handleStatsOpen = (e) => {
     showStats ? setShowStats(false) : setShowStats(true);
   };
@@ -29,7 +29,6 @@ export const PokeScreen = () => {
     showAbility ? setShowAbility(false) : setShowAbility(true);
   };
 
-  
   const {
     id,
     sprites,
@@ -42,134 +41,149 @@ export const PokeScreen = () => {
     weight,
     ...siglePokeData
   } = singlePokeData;
-  
+
   return (
     <>
-    {
-      sprites ? (
+      {sprites ? (
         <div>
-      <div className="imgDiv">
-      <div className="animate__animated animate__fadeInLeft">
-      <img
-      className="imgClass"
-      src={
-        sprites?.other["official-artwork"].front_default ||
-        sprites?.other["home"].front_default
-      }
-      alt={name}
-      ></img>
-      </div>
-      <div className="animate__animated animate__fadeIn">
-      <h1>{name ? name[0].toUpperCase() + name?.substring(1) : null}</h1>
-      <h2>ID: {id}</h2>
-      <h2>height: {height}</h2>
-      <h2>weight: {weight}</h2>
-      <div className="typosEnPokeScreen" style={{ position: "relative" }}>
-      {types?.map(({ type: { name } }, index) => {
-        return (
-          <div
-          key={index}
-          className="type"
-          style={{ backgroundColor: colorTipos(name) }}
-          >
-          <p key={index} className="typeText">
-          {name.toUpperCase()}
-          </p>
-          </div>
-          );
-        })}
-        </div>
-        
-          <button
-            style={{ display: "flex", justifyContent: "center" }}
-            onClick={handleReturn}
-            size="small"
-            color="primary"
+          <div className="margin-button">
+            <button
+              onClick={handleReturn}
+              size="small"
+              color="primary"
             >
-            back
-          </button>
-        </div>
-        </div>
-        <div style={{marginLeft: 50, marginTop: 50}}>
-        <div>
-          <button className="pokeScreenButtons" onClick={handleAbilityOpen}>Abilities:</button>
-          {showAbility && (
-            <ul>
-            {abilities?.map(({ ability, index }) => {
-              return (
-                <li
-                style={{ color: "black", width: `100%` }}
-                key={ability.name}
-                >
-                  {ability.name}
-                </li>
-              );
-            })}
-          </ul>
-            )}
-        </div>
-        <div>
-          <button className="pokeScreenButtons" onClick={handleStatsOpen}>Stats:</button>
-          {showStats && (
-            <ul>
-              {stats?.map((stat, index) => {
-                return   (
-                  <li style={{color: 'black', textAlign: 'center'}} key={index}>
-                    {stat.stat.name.toUpperCase()}
+              Back
+            </button>
+          </div>
+          <div className="imgDiv">
+            <div className="animate__animated animate__fadeInLeft">
+              <img
+                className="imgClass"
+                src={
+                  sprites?.other["official-artwork"].front_default ||
+                  sprites?.other["home"].front_default
+                }
+                alt={name}
+              ></img>
+            </div>
+            <div className="animate__animated animate__fadeIn">
+              <h1>
+                #{id} {name ? name[0].toUpperCase() + name?.substring(1) : null}
+              </h1>
+              <div
+                className="typosEnPokeScreen"
+                style={{ position: "relative" }}
+              >
+                {types?.map(({ type: { name } }, index) => {
+                  return (
                     <div
-                      className="borderDiv"
-                      style={{
-                        backgroundColor: "white",
-                        width: 255,
-                        marginLeft: 0,
-                        borderRadius: 10,
-                      }}
-                      >
-                      <div
-                        style={{
-                          backgroundColor: "#1976d2",
-                          width: `${stat["base_stat"]}px`,
-                          marginLeft: -1,
-                          color: "white",
-                          borderRadius: 10,
-                        }}
-                        >
-                        <span
-                          style={{
-                            fontSize: `${stat["base_stat"]}` <= 25 && 12,
-                          }}
-                          >
-                          {stat["base_stat"]}
-                        </span>
-                      </div>
+                      key={index}
+                      className="type"
+                      style={{ backgroundColor: colorTipos(name) }}
+                    >
+                      <p key={index} className="typeText">
+                        {name.toUpperCase()}
+                      </p>
                     </div>
-                  </li>
-                );;
-              })}
-            </ul>
-          )}
+                  );
+                })}
+              </div>
+              <div className="div-measures">
+                <p>Height: {height / 10} m</p>
+                <p>Weight: {weight / 10} kg</p>
+              </div>
+            </div>
+          </div>
+          <div style={{ marginLeft: 50, marginTop: 50 }}>
+            <div>
+              <button className="pokeScreenButtons" onClick={handleAbilityOpen}>
+                Abilities:
+              </button>
+              {showAbility && (
+                <ul>
+                  {abilities?.map(({ ability }) => {
+                    console.log(ability)
+                    return (
+                      <li
+                        style={{ color: "black", width: `100%` }}
+                        key={ability.name}
+                      >
+                        {ability.name}
+                      </li>
+                    );
+                  })}
+                </ul>
+              )}
+            </div>
+            <div>
+              <button className="pokeScreenButtons" onClick={handleStatsOpen}>
+                Stats:
+              </button>
+              {showStats && (
+                <ul>
+                  {stats?.map((stat, index) => {
+                    return (
+                      <li
+                        style={{ color: "black", textAlign: "center" }}
+                        key={index}
+                      >
+                        {stat.stat.name.toUpperCase()}
+                        <div
+                          className="borderDiv"
+                          style={{
+                            backgroundColor: "white",
+                            width: 255,
+                            marginLeft: 0,
+                            borderRadius: 10,
+                          }}
+                        >
+                          <div
+                            style={{
+                              backgroundColor: "#1976d2",
+                              width: `${stat["base_stat"]}px`,
+                              marginLeft: -1,
+                              color: "white",
+                              borderRadius: 10,
+                            }}
+                          >
+                            <span
+                              style={{
+                                fontSize: `${stat["base_stat"]}` <= 25 && 12,
+                              }}
+                            >
+                              {stat["base_stat"]}
+                            </span>
+                          </div>
+                        </div>
+                      </li>
+                    );
+                  })}
+                </ul>
+              )}
+            </div>
+            <div>
+              <button className="pokeScreenButtons" onClick={handleMovesOpen}>
+                Moves:
+              </button>
+              {showMoves && (
+                <ul>
+                  {moves?.map(({ move }, index) => {
+                    return (
+                      <li style={{ color: "black", width: `100%` }} key={index}>
+                        {move.name}
+                      </li>
+                    );
+                  })}
+                </ul>
+              )}
+            </div>
+          </div>
         </div>
-        <div>
-          <button className="pokeScreenButtons" onClick={handleMovesOpen}>Moves:</button>
-          {showMoves && (
-            <ul>
-              {moves?.map(({ move }, index) => {
-                return (
-                  <li style={{ color: "black", width: `100%` }} key={index}>
-                    {move.name}
-                  </li>
-                );
-              })}
-            </ul>
-          )}
-        </div>
-      </div>
-    </div>
-  )
-  : <Spinner />
-} 
-  </>
-);
+      ) : (
+        <Spinner />
+      )}
+    </>
+  );
 };
 
 export default PokeScreen;
